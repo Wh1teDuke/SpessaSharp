fbdaeb31e7e78002af9b704d4447f52f9b3e2f0a
WhiteDuke <whiteduke@protonmail.com>
2026-06-16 21:14:01 +0200

    update RealtimeMidiSynth.cs

--- Examples/RealtimeMidiSynth.cs
+++ Examples/RealtimeMidiSynth.cs
@@ -2,7 +2,7 @@
 
 #:project ../SpessaSharp/SpessaSharp.csproj
 #:package OwnAudioSharp.Midi@3.1.0
-#:package OwnAudioSharp.Basic@3.1.0
+#:package OwnAudioSharp.Basic@3.1.3
 
 using System.Diagnostics;
 using Ownaudio.Core;
@@ -16,25 +16,32 @@ using SpessaSharp.Synthesizer;
 // Process arguments
 if (args.Length < 1)
 {
-    // Also ./RealtimeMidiSynth.cs <soundbank path> [device]
-    Console.WriteLine("Usage: dotnet run RealtimeMidiSynth.cs -- <soundbank path> [device]");
+    // Also ./RealtimeMidiSynth.cs <soundbank path> <sample rate> [device]
+    Console.WriteLine("Usage: dotnet run RealtimeMidiSynth.cs -- <soundbank path> [sample rate] [device]");
     Environment.Exit(1);
 }
 
 // Initialize the synthesizer
-const int sampleRate = 44_100;
-Console.WriteLine("Initializing synthesizer ...");
 var sfPath = args[0];
-var sf = new FileInfo(sfPath);
+var sampleRate = args.Length > 1 ? int.Parse(args[1]) : 44_100;
+// Quantum size is recommended to be 128 at 48kHz, 48000 / 375 = 128
+// Scale this to the selected sample rate
+var quantum = (int)Math.Round(sampleRate / 375f);
 
+Console.WriteLine("Initializing synthesizer ...");
 var synth = new SpessaSharpProcessor(
     sampleRate, Synthesizer.Options.Default with
-    { EventsEnabled = false, });
+    {
+        EventsEnabled = false,
+        MaxBufferSize = quantum,
+    });
 
+// Load sound bank
+var sf = new FileInfo(sfPath);
 synth.SoundBankManager.Add(SoundBank.From(sf), "main");
 
 // Initialize the MIDI inputs
-var device = args.Length > 1
+var device = args.Length > 2
     ? args[1] : MidiPortFactory.GetInputPortNames()[0];
 
 Console.WriteLine($"Listening on '{device}'");
@@ -62,10 +69,8 @@ OwnaudioNet.Initialize(config);
 OwnaudioNet.Start();
 
 // Initialize the audio stream
-const int quantum = 64;
-const int blockSize = 4;
-var left = new float[quantum * blockSize];
-var right = new float[quantum * blockSize];
+var left = new float[quantum];
+var right = new float[quantum];
 var interleaved = new float[left.Length * 2];
 var watch = Stopwatch.StartNew();
 var start = watch.Elapsed;
@@ -76,7 +81,8 @@ while (true)
     var currentTime = 0d;
     lock (synthLock) currentTime = synth.CurrentTime;
 
-    if (currentTime - t > 0.5)
+    // Keep rendering until we are 0.1 seconds ahead of time
+    if (currentTime - t >= .1)
     {
         Thread.Sleep(1);
         continue;
@@ -84,15 +90,7 @@ while (true)
     
     left.AsSpan().Clear();
     right.AsSpan().Clear();
-    var write = 0;
-    lock(synthLock)
-    {
-        for (var i = 0; i < blockSize; i++)
-        {
-            synth.Process(left, right, write, quantum);
-            write += quantum;
-        }
-    }
+    lock(synthLock) synth.Process(left, right);
 
     for (var i = 0; i < left.Length; i++)
     {
