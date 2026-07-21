af03e86b4391651916d3e57ba333688e336f51b1
WhiteDuke <whiteduke@protonmail.com>
2026-07-21 19:56:58 +0200

    test.sh: Use sox to compare rendered wavs.

--- Tests/test.sh
+++ Tests/test.sh
@@ -23,8 +23,24 @@ if [ ! -d "generated" ]; then
   echo "Setup ..."
   mkdir generated
   cd generated
-  ../../.github/scripts/ssdiff/setup_ss.sh
+
+  echo "  Setup Bun"
+  wget -q "https://github.com/oven-sh/bun/releases/download/canary/bun-linux-x64.zip"
+  unzip -q bun-linux-x64.zip
+  mv bun-linux-x64/bun .
+  rm -d bun-linux-x64/
+  rm bun-linux-x64.zip
+  chmod +x ./bun
+  BUN_INSTALL_CACHE_DIR=bcc ./bun init -y --silent > /dev/null 2>&1
+
+  echo "  Clone SpessaSynth"
   git clone -q --depth 1 "https://github.com/spessasus/spessasynth_core.git"
+
+  echo "  Download Sound bank (GeneralUser-GS)"
+  wget -q "https://github.com/mrbumpy409/GeneralUser-GS/raw/refs/heads/main/GeneralUser-GS.sf2"
+  
+  echo ""
+  
 else
   cd generated
 fi
@@ -43,35 +59,63 @@ test() {
   local ssynth="spessasynth_core/tests/$2"
   local result="$3"
 
-  local ssharp_out="${result}"
-  local ssynth_out="spessasynth_core/tests/midi_file/generated/${result}"
+  local ssharp_mid_out="${result}"
+  local ssynth_mid_out="spessasynth_core/tests/midi_file/generated/${result}"
+  
+  local ssharp_wav_out="${ssharp_mid_out/%.mid/.wav}"
+  local ssynth_wav_out="${ssynth_mid_out/%.mid/.wav}"
+  
+  local bank="GeneralUser-GS.sf2" # TODO: Pass as (optional) argument
 
   # Invalidate prev run
-  rm -f "$ssharp_out"
-  rm -f "$ssynth_out"
+  rm -f "${ssharp_mid_out}" "${ssharp_wav_out}"
+  rm -f "${ssynth_mid_out}" "${ssynth_wav_out}"
 
-  printf "%-40s" "$(printf "%3d" "${test_total}")) $result ..."
+  printf "%-40s" "$(printf "%3d" "${test_total}")) ${result} ..."
 
   # Execute both spessasynth and spessasharp
+  #   SSynth
   BUN_INSTALL_CACHE_DIR=bcc ./bun run --silent "$ssynth" > /dev/null
+  #   SSharp
   dotnet "$ssharp" > /dev/null
-
-  for file in "$ssharp_out" "$ssynth_out"; do
-    if [ ! -f "$file" ]; then
-      echo "!$file Not found!"
+  
+  # Render both (Use ssynth mid as reference)
+  #   SSynth
+  BUN_INSTALL_CACHE_DIR=bcc ./bun run --silent ./spessasynth_core/examples/midi_to_wav_node.ts "$bank" "${ssynth_mid_out}" "${ssynth_wav_out}" > /dev/null
+  #   SSharp
+  dotnet run -c Release ../../Examples/MidiToWavNode.cs -- "$bank" "${ssynth_mid_out}" "${ssharp_wav_out}" > /dev/null
+  
+  for file in "${ssharp_mid_out}" "${ssharp_wav_out}" "${ssynth_mid_out}" "${ssynth_wav_out}"; do
+    if [ ! -f "${file}" ]; then
+      echo "!${file} Not found!"
       test_fail=$((test_fail+1))
       failed_tests+=("$result")
       return
     fi
   done
 
-  # Compare
-  if cmp -s "$ssharp_out" "$ssynth_out"; then
+  # 1 Compare Midi
+  if cmp -s "${ssharp_mid_out}" "${ssynth_mid_out}"; then
+    test_success=$((test_success+1))
+    echo -n "✓   "
+  else
+    test_fail=$((test_fail+1))
+    failed_tests+=("${ssharp_mid_out}")
+    echo - "✗   "
+  fi
+  
+  # 2 Compare wav
+  local threshold=0.1 # TODO: pass optional tresh
+  local max_amp=$(
+    sox -m -v 1 "${ssharp_wav_out}" -v -1 "${ssynth_wav_out}" -n stat 2>&1 |
+    awk '/Maximum amplitude:/ {print $3}'
+  )
+  if (( $(echo "$max_amp < $threshold" | bc -l) )); then
     test_success=$((test_success+1))
     echo "✓"
   else
     test_fail=$((test_fail+1))
-    failed_tests+=("$result")
+    failed_tests+=("${ssharp_wav_out} ($max_amp)")
     echo "✗"
   fi
 }
@@ -79,14 +123,14 @@ test() {
 # -------------------------------------
 # TEST START
 echo "Test start ..."
-echo ""
+echo " Num Name                              mid wav"
 
 # Midi
-test midi/cc/SoftPedal.cs midi_file/cc/soft_pedal.ts soft_pedal_test.mid
-test midi/cc/RPNFineTuning.cs midi_file/cc/rpn_fine_tuning.ts rpn_fine_tuning_test.mid
-test midi/cc/RealtimeRPNTuning.cs midi_file/cc/realtime_rpn_tuning.ts rpn_tuning_real-time_test.mid
+test "midi/cc/SoftPedal.cs" "midi_file/cc/soft_pedal.ts" "soft_pedal_test.mid"
+test "midi/cc/RPNFineTuning.cs" "midi_file/cc/rpn_fine_tuning.ts" "rpn_fine_tuning_test.mid"
+test "midi/cc/RealtimeRPNTuning.cs" "midi_file/cc/realtime_rpn_tuning.ts" "rpn_tuning_real-time_test.mid"
 
-test midi/other/AssignMode.cs midi_file/other/assign_mode.ts assign_mode_test.mid
+test "midi/other/AssignMode.cs" "midi_file/other/assign_mode.ts" "assign_mode_test.mid"
 
 # -------------------------------------
 
@@ -94,7 +138,7 @@ test midi/other/AssignMode.cs midi_file/other/assign_mode.ts assign_mode_test.mi
 echo ""
 echo "$test_total Tests. $test_success passed and $test_fail failed."
 for failed in "${failed_tests[@]}"; do
-  echo "   ✗ $failed"
+  echo "  ✗  ${failed}"
 done
 
 [ "$test_fail" -eq 0 ]
